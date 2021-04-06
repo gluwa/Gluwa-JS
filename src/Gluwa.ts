98b13622a79e466dd7e6157e1646a20fba66be63
hyeonggook.lee <hyeonggook.lee@gluwa.com>
2021-04-07 04:32:32 +0900

    To fix sNGNG using 18 decimals

--- src/Gluwa.ts
+++ src/Gluwa.ts
@@ -160,10 +160,7 @@ export default class Gluwa {
     try {
       let amount = ethers.utils.parseEther(args.Amount).toString();
       let fee = ethers.utils.parseEther(args.Fee).toString();
-      if (
-        args.Currency === 'sUSDCG' ||
-        args.Currency === 'sNGNG'
-      ) {
+      if (args.Currency === 'sUSDCG') {
         amount = amount.substr(0, amount.length - 12);
         fee = fee.substr(0, fee.length - 12);
       }
