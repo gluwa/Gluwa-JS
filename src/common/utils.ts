1ede3b7377616679dc6578c642c11d837b2e1f09
yiseul.jo <>
2021-05-21 06:33:34 +0900

    GLA-133  |  Update node.js SDK to Generate Longer Nonce

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,21 +1,15 @@
 global.crypto = require('crypto');
 
 export const getStringOfRandomNumbers = (length) => {
-  // const places = 10 ** length;
-  // // crypto.randomInt([min, ]max) returns min(default; 0) <= n < max
-  // let num = crypto.randomInt(places).toString();
   let num = '';
+
   for (let i = 0; i < length; i += 1) {
+    // crypto.randomInt([min, ]max) returns min(default; 0) <= n < max
     num += crypto.randomInt(10).toString();
   }
 
-  // put leading zeros
-  while (num.length < length) {
-    num = `0${num}`;
-  }
-
   if (num.charAt(0) === '0') {
-    const tmp = (crypto.randomInt(9) + 1).toString();
+    const tmp = crypto.randomInt(1, 10).toString();
     num.replace('0', tmp);
   }
 
